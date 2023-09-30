c81292c184a9178598fc3e40cc41c97731b79bad
Youssef Flija <75099727+Flija0@users.noreply.github.com>
2023-09-30 17:34:47 +0100

    Update chatbox.blade.php

--- troc/resources/views/livewire/chat/chatbox.blade.php
+++ troc/resources/views/livewire/chat/chatbox.blade.php
@@ -15,9 +15,7 @@
             }}</div>
 
         <div class="info text-center p-1 mt-auto mr-0 mb-auto ml-auto font-normal flex flex-nowrap">
-            <div class="info_item cursor-pointer my-1 mx-3">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#000000" viewBox="0 0 256 256"><path d="M231.88,175.08A56.26,56.26,0,0,1,176,224C96.6,224,32,159.4,32,80A56.26,56.26,0,0,1,80.92,24.12a16,16,0,0,1,16.62,9.52l21.12,47.15,0,.12A16,16,0,0,1,117.39,96c-.18.27-.37.52-.57.77L96,121.45c7.49,15.22,23.41,31,38.83,38.51l24.34-20.71a8.12,8.12,0,0,1,.75-.56,16,16,0,0,1,15.17-1.4l.13.06,47.11,21.11A16,16,0,0,1,231.88,175.08Z"></path></svg>
-            </div>
+           
         
                 <div class="info_item cursor-pointer hover:text-neutral-800 my-1 mx-3">
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#000000" viewBox="0 0 256 256"><path d="M216,40H40A16,16,0,0,0,24,56V200a16,16,0,0,0,16,16H216a16,16,0,0,0,16-16V56A16,16,0,0,0,216,40ZM156,88a12,12,0,1,1-12,12A12,12,0,0,1,156,88ZM40,200V172l52-52,80,80Zm176,0H194.63l-36-36,20-20L216,181.38V200Z"></path></svg>
