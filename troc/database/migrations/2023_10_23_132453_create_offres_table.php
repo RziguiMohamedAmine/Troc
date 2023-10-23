78e845ab14db43baea100c58f5985f3212d93917
Amin-Dhouib <84295527+Zied-Mabrouk@users.noreply.github.com>
2023-10-23 14:39:00 +0100

    Update 2023_10_23_132453_create_offres_table.php

--- troc/database/migrations/2023_10_23_132453_create_offres_table.php
+++ troc/database/migrations/2023_10_23_132453_create_offres_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('offres', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
 
             $table->unsignedBigInteger("product_id");
             $table->unsignedBigInteger("user_id");
